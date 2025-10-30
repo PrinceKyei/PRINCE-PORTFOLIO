872b9d56f45928e2ad2bb89f4ba2749908455fe8
Prince Doku-Kyei <princekyei080@gmail.com>
2025-10-30 15:15:05 +0000

    Add files via upload

---
+++ Task 6.js
@@ -0,0 +1,7 @@
+// Mobile menu toggle
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.getElementById("nav-menu");
+
+hamburger.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+});
